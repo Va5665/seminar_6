fd5f865d3b8fc5531fff1cdae8a3253cb8d50c8d
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-08-29 15:14:19 +0530

    добаваили

---
+++ 41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры числа через запятую. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//  0, 7, 8, -2, -2 -> 2 -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите элементы(через запятую и пробел): ");
+string input = Console.ReadLine();
+string[] numberStrings = input.Split(",");
+int[] arr = Array.ConvertAll(numberStrings, int.Parse);
+
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
